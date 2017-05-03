3e4ff16a707686fefeaa8e795f6d40508af52a7b
Daniel Mucka <dady8889@gmail.com>
2017-05-03 22:34:19 +0200

    Added variables that are needed
    Change-Id: Iad6876d8a74ac7892776c913a48ce2822e1e74d1

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -1,12 +1,21 @@
-#Android makefile to build kernel as a part of Android Build
-PERL		= perl
+# Android makefile to build kernel as a part of Android Build
+
+LOCAL_PATH := $(call my-dir)
+
+OBB_PRODUCT_NAME := hi6250
+BALONG_TOPDIR := $(LOCAL_PATH)/drivers/vendor/hisi
+CFG_PLATFORM := hi6250
+TARGET_ARM_TYPE := arm64
+export BALONG_TOPDIR OBB_PRODUCT_NAME CFG_PLATFORM TARGET_ARM_TYPE
+
+PERL := perl
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 
 KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
 TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/arm64/boot/zImage
-KERNEL_IMG=$(KERNEL_OUT)/arch/arm64/boot/Image
+KERNEL_IMG := $(KERNEL_OUT)/arch/arm64/boot/Image
 
 ifeq ($(TARGET_USES_UNCOMPRESSED_KERNEL),true)
 $(info Using uncompressed kernel)
@@ -15,11 +24,11 @@ else
 TARGET_PREBUILT_KERNEL := $(TARGET_PREBUILT_INT_KERNEL)
 endif
 
-COMMON_HEAD := $(shell pwd)/kernel/drivers/
-COMMON_HEAD += $(shell pwd)/kernel/mm/
-COMMON_HEAD += $(shell pwd)/kernel/include/hisi/
-COMMON_HEAD += $(shell pwd)/external/efipartition	
-COMMON_HEAD += $(shell pwd)/drivers/vendor/hisi/ap/platform/hi6250/
+COMMON_HEAD := $(LOCAL_PATH)/kernel/drivers/
+COMMON_HEAD += $(LOCAL_PATH)/kernel/mm/
+COMMON_HEAD += $(LOCAL_PATH)/kernel/include/hisi/
+COMMON_HEAD += $(LOCAL_PATH)/external/efipartition
+COMMON_HEAD += $(LOCAL_PATH)/drivers/vendor/hisi/ap/platform/hi6250/
 
 ifneq ($(COMMON_HEAD),)
 BALONG_INC := $(patsubst %,-I%,$(COMMON_HEAD))
@@ -56,9 +65,9 @@ HISI_MDRV_GCOV_DEFCONFIG := ${KERNEL_ARCH_ARM_CONFIGS}/gcov_defconfig
 APPEND_MODEM_GCOV_DEFCONFIG := cat $(HISI_MDRV_GCOV_DEFCONFIG) >> $(KERNEL_GEN_CONFIG_PATH)
 endif
 
-idl_tool_script_path := $(shell pwd)/kernel/scripts/kernel_modem_idl_tool.py
-driver_hisi_modem_out_dir := $(shell pwd)/$(KERNEL_OUT)/drivers/hisi/modem
-kernel_driver_hisi_dir := $(shell pwd)/kernel/drivers/hisi
+idl_tool_script_path := $(LOCAL_PATH)/kernel/scripts/kernel_modem_idl_tool.py
+driver_hisi_modem_out_dir := $(LOCAL_PATH)/$(KERNEL_OUT)/drivers/hisi/modem
+kernel_driver_hisi_dir := $(LOCAL_PATH)/kernel/drivers/hisi
 
 $(KERNEL_OUT):
 	mkdir -p $(KERNEL_OUT)
---
+++ tools/gator/daemon/SrcMd5.cpp
@@ -0,0 +1,1 @@
+extern const char *const gSrcMd5 = "714be5f8789bf3ab4ae5ea161e9478b5";
