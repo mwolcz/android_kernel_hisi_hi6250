df77e06fa0d447fda92f14d9f40fca6ac601323e
Daniel Mucka <dady8889@gmail.com>
2017-05-06 23:19:08 +0200

    Fixed kernel build
    AndroidKernel.mk - builds automatically from platform build
    pmom_cat - fixed compiler error
    
    Change-Id: I2dacc0a7e2620d4e6a6ff33cc881dd6a3686008b

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -1,34 +1,46 @@
 # Android makefile to build kernel as a part of Android Build
-
-LOCAL_PATH := $(call my-dir)
+# http://jhshi.me/2014/06/30/build-kernel-in-tree-with-aosp-for-nexus-5-hammerhead/
 
 OBB_PRODUCT_NAME := hi6250
-BALONG_TOPDIR := $(LOCAL_PATH)/drivers/vendor/hisi
+BALONG_TOPDIR := $(KERNEL_PATH_FULL)/drivers/vendor/hisi
 CFG_PLATFORM := hi6250
 TARGET_ARM_TYPE := arm64
-export BALONG_TOPDIR OBB_PRODUCT_NAME CFG_PLATFORM TARGET_ARM_TYPE
-
+SRCHI1101 := drivers/misc/hw-drv
 PERL := perl
+export BALONG_TOPDIR OBB_PRODUCT_NAME CFG_PLATFORM TARGET_ARM_TYPE SRCHI1101 PERL
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 
+KERNEL_HEADER_DEFCONFIG := $(strip $(KERNEL_HEADER_DEFCONFIG))
+ifeq ($(KERNEL_HEADER_DEFCONFIG),)
+KERNEL_HEADER_DEFCONFIG := $(KERNEL_DEFCONFIG)
+endif
+
 KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
-KERNEL_CONFIG := $(KERNEL_OUT)/.config
-TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/arm64/boot/zImage
-KERNEL_IMG := $(KERNEL_OUT)/arch/arm64/boot/Image
+KERNEL_OUT_FULL := $(shell pwd)/$(KERNEL_OUT)
+KERNEL_CONFIG := $(KERNEL_OUT_FULL)/.config
+
+KERNEL_ARCH_PREFIX := arm64
+CROSS_COMPILE_PREFIX := aarch64-linux-android-
+
+ifeq ($(KERNEL_DEFCONFIG)$(wildcard $(KERNEL_CONFIG)),)
+$(error Kernel configuration not defined, cannot build kernel)
+else
 
 ifeq ($(TARGET_USES_UNCOMPRESSED_KERNEL),true)
 $(info Using uncompressed kernel)
-TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/piggy
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH_PREFIX)/boot/Image
 else
-TARGET_PREBUILT_KERNEL := $(TARGET_PREBUILT_INT_KERNEL)
+TARGET_PREBUILT_INT_KERNEL := $(KERNEL_OUT)/arch/$(KERNEL_ARCH_PREFIX)/boot/Image.gz
 endif
 
-COMMON_HEAD := $(LOCAL_PATH)/kernel/drivers/
-COMMON_HEAD += $(LOCAL_PATH)/kernel/mm/
-COMMON_HEAD += $(LOCAL_PATH)/kernel/include/hisi/
-COMMON_HEAD += $(LOCAL_PATH)/external/efipartition
-COMMON_HEAD += $(LOCAL_PATH)/drivers/vendor/hisi/ap/platform/hi6250/
+TARGET_PREBUILT_KERNEL := $(TARGET_PREBUILT_INT_KERNEL)
+
+COMMON_HEAD := $(KERNEL_PATH_FULL)/kernel/drivers/
+COMMON_HEAD += $(KERNEL_PATH_FULL)/kernel/mm/
+COMMON_HEAD += $(KERNEL_PATH_FULL)/kernel/include/hisi/
+COMMON_HEAD += $(KERNEL_PATH_FULL)/drivers/vendor/hisi/ap/platform/hi6250/
+COMMON_HEAD += $(KERNEL_PATH_FULL)/external/efipartition
 
 ifneq ($(COMMON_HEAD),)
 BALONG_INC := $(patsubst %,-I%,$(COMMON_HEAD))
@@ -42,56 +54,30 @@ endif
 
 export BALONG_INC
 
-ifneq ($(filter hi3650 hi3650emulator, $(TARGET_BOARD_PLATFORM)),)
-ifeq ($(strip $(CFG_HISI_MINI_AP)), false)
-	APPEND_MODEM_DEFCONFIG := cat $(HISI_3650_MODEM_DEFCONFIG) >> $(KERNEL_GEN_CONFIG_PATH)
-endif
-endif
-
-ifneq ($(filter hi6250, $(TARGET_BOARD_PLATFORM)),)
-ifeq ($(strip $(CFG_HISI_MINI_AP)), false)
-	APPEND_MODEM_DEFCONFIG := cat $(HISI_6250_MODEM_DEFCONFIG) >> $(KERNEL_GEN_CONFIG_PATH)
-endif
-endif
-
-ifneq ($(filter hi3660, $(TARGET_BOARD_PLATFORM)),)
-ifeq ($(strip $(CFG_HISI_MINI_AP)), false)
-	APPEND_MODEM_DEFCONFIG := cat $(HISI_3660_MODEM_DEFCONFIG) >> $(KERNEL_GEN_CONFIG_PATH)
-endif
-endif
-
-ifeq ($(strip $(llt_gcov)),y)
-HISI_MDRV_GCOV_DEFCONFIG := ${KERNEL_ARCH_ARM_CONFIGS}/gcov_defconfig
-APPEND_MODEM_GCOV_DEFCONFIG := cat $(HISI_MDRV_GCOV_DEFCONFIG) >> $(KERNEL_GEN_CONFIG_PATH)
-endif
-
-idl_tool_script_path := $(LOCAL_PATH)/kernel/scripts/kernel_modem_idl_tool.py
-driver_hisi_modem_out_dir := $(LOCAL_PATH)/$(KERNEL_OUT)/drivers/hisi/modem
-kernel_driver_hisi_dir := $(LOCAL_PATH)/kernel/drivers/hisi
-
 $(KERNEL_OUT):
 	mkdir -p $(KERNEL_OUT)
 
 $(KERNEL_CONFIG): $(KERNEL_OUT)
-	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- $(KERNEL_DEFCONFIG)
-
-$(KERNEL_OUT)/piggy : $(TARGET_PREBUILT_INT_KERNEL)
-	$(hide) gunzip -c $(KERNEL_OUT)/arch/arm/boot/compressed/piggy.gzip > $(KERNEL_OUT)/piggy
+	$(MAKE) -C $(KERNEL_DIR) O=$(KERNEL_OUT_FULL) ARCH=$(KERNEL_ARCH_PREFIX) CROSS_COMPILE=$(CROSS_COMPILE_PREFIX) $(KERNEL_DEFCONFIG)
 
 $(TARGET_PREBUILT_INT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG) $(KERNEL_HEADERS_INSTALL)
-	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm64 CROSS_COMPILE=aarch64-linux-android-
+	$(hide) echo "Building kernel..."
+	$(MAKE) -C $(KERNEL_DIR) O=$(KERNEL_OUT_FULL) ARCH=$(KERNEL_ARCH_PREFIX) CROSS_COMPILE=$(CROSS_COMPILE_PREFIX)
 
-$(KERNEL_HEADERS_INSTALL): $(KERNEL_OUT) $(KERNEL_CONFIG)
-	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- headers_install
+$(KERNEL_HEADERS_INSTALL): $(KERNEL_OUT)
+	rm -f $(KERNEL_CONFIG)
+	$(MAKE) -C $(KERNEL_DIR) O=$(KERNEL_OUT_FULL) ARCH=$(KERNEL_ARCH_PREFIX) CROSS_COMPILE=$(CROSS_COMPILE_PREFIX) $(KERNEL_HEADER_DEFCONFIG)
+	$(MAKE) -C $(KERNEL_DIR) O=$(KERNEL_OUT_FULL) ARCH=$(KERNEL_ARCH_PREFIX) CROSS_COMPILE=$(CROSS_COMPILE_PREFIX) headers_install
 
 kerneltags: $(KERNEL_OUT) $(KERNEL_CONFIG)
-	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- tags
+	$(MAKE) -C $(KERNEL_DIR) O=$(KERNEL_OUT_FULL) ARCH=$(KERNEL_ARCH_PREFIX) CROSS_COMPILE=$(CROSS_COMPILE_PREFIX) tags
 
 kernelconfig: $(KERNEL_OUT) $(KERNEL_CONFIG)
 	env KCONFIG_NOTIMESTAMP=true \
-	     $(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- menuconfig
+		$(MAKE) -C $(KERNEL_DIR) O=$(KERNEL_OUT_FULL) ARCH=$(KERNEL_ARCH_PREFIX) CROSS_COMPILE=$(CROSS_COMPILE_PREFIX) menuconfig
 	env KCONFIG_NOTIMESTAMP=true \
-	     $(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- savedefconfig
-	cp $(KERNEL_OUT)/defconfig kernel/arch/arm64/configs/$(KERNEL_DEFCONFIG)
+		$(MAKE) -C $(KERNEL_DIR) O=$(KERNEL_OUT_FULL) ARCH=$(KERNEL_ARCH_PREFIX) CROSS_COMPILE=$(CROSS_COMPILE_PREFIX) savedefconfig
+	cp $(KERNEL_OUT_FULL)/defconfig $(KERNEL_PATH_FULL)/arch/$(KERNEL_ARCH_PREFIX)/configs/$(KERNEL_DEFCONFIG)
 
+endif
 endif
